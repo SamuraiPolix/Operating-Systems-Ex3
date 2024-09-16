73902c1b9ef2cfadfea5e34b04869ae5748c501e
SamuraiPolix <sam.lazareanu@gmail.com>
2024-09-16 12:31:46 +0300

    larger test to compare list and deque

--- q2/inputs/generate_gprof_inputs.py
+++ q2/inputs/generate_gprof_inputs.py
@@ -14,6 +14,6 @@ def generate_input_file(filename, implementation, n, m):
             f.write(f"{u} {v}\n")
 
 # Large inputs
-generate_input_file("input_large_deque.txt", "deque", 10000, 30000)
-generate_input_file("input_large_adjmatrix.txt", "adjmatrix", 10000, 30000)
-generate_input_file("input_large_list.txt", "list", 10000, 30000)
+generate_input_file("input_large_deque.txt", "deque", 100000, 300000)
+generate_input_file("input_large_adjmatrix.txt", "adjmatrix", 100000, 300000)
+generate_input_file("input_large_list.txt", "list", 100000, 300000)
